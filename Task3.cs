e71af88edd9bae1638693f9f2826526c39ce4ecc
MatkarimovAlexander <156626641+MatkarimovAlexander@users.noreply.github.com>
2026-09-07 23:30:59 +0300

    Task 3

---
+++ Task3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOAP_3
+{
+    internal class Task3
+    {
+        // Класс Matrix - Матрица, поле боя
+        // Класс Cell ячейка имеющая подтип(ABCDE)
+        // Класс Бонусов\Проклятий(отдельная сущность на которую могут ссылаться несколько ячеек)
+        // Класс EventPipeline обрабатывающий очередь событий(ввод пользователя, заполнение матрицы после взрыва, вывод анимаций...)
+    }
+}
