73ae0bef60baa5b990fb372d383c733837dd858f
MatkarimovAlexander <156626641+MatkarimovAlexander@users.noreply.github.com>
2026-09-10 23:04:08 +0300

    task4

---
+++ Task4.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOAP_3
+{
+    internal class Task4
+    {
+        // Возможно какаято итеративная структура, которая выполняет рекурсивно действия, которые вызывают разные абстрактные модули.
+        // List<GameAction>
+    }
+
+    public class GameAction
+    {
+        List<GameAction> actions;
+        public void currentAction() 
+        {
+            // some action
+            foreach (var item in actions)
+            {
+                item.currentAction();
+            }
+        }
+    }
+}
