891fbb6da5fffa522b4e00d94f9a8a7c0f985393
MatkarimovAlexander <156626641+MatkarimovAlexander@users.noreply.github.com>
2026-09-13 23:26:33 +0300

    ворк

---
+++ Task5.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOAP_3
+{
+    public class Task5
+    {
+        // 1. То что было задумано ранее по факту паттерн Reactor (Класс EventPipeline обрабатывающий очередь событий(ввод пользователя, заполнение матрицы после взрыва, вывод анимаций...))
+        // 2. EventSourcing, чтобы хранился порядок обработанных событий, и у нас была всегда актуальная матрица которую мы покажем и будем ожидать ввод пользователя
+        // 3. Outbox - чтобы не возникали баги с дублированием обработки логики
+    }
+}
