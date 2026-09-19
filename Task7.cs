9e0ff99e3fc61dd81b3a1147f84b9e6698bb80e6
MatkarimovAlexander <156626641+MatkarimovAlexander@users.noreply.github.com>
2026-09-19 21:29:52 +0300

    task7

---
+++ Task7.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOAP_3
+{
+    internal class Task7
+    {
+        // Кластер Обработки событий
+        // Кластер вывода информации
+        // Кластер ввода информации
+        // Кластер CRUD таблицы
+    }
+}
